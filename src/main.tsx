6cd69803b53ea5b3d0d8d9d5ab6f3ddf99aed57a
m0khina <artikovamohina.03@gmail.com>
2025-01-28 16:54:06 +0300

    init: feat/ UI layout has been created

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import DashboardLayout from '@/components/layouts/dashboard-layout.tsx'
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Home</h1>,
+    },
+    {
+        path: 'login',
+        element: <h1>Login</h1>,
+    },
+    {
+        path: 'register',
+        element: <h1>Register</h1>,
+    },
+    {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+            {
+                index: true,
+                element: <h1>Dashboard main page</h1>,
+            },
+        ],
+    },
+])
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 )
